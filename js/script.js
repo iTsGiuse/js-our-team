d1503d09ecec49d01e5460ab75e0b7f6f47cde3a
iTsGiuse <giuse.amato@icloud.com>
2024-03-20 15:35:44 +0100

    feat: add array di oggetti nello script

--- js/script.js
+++ js/script.js
@@ -11,3 +11,36 @@ Trasformare la stringa foto in una immagine effettiva
 BONUS 2:
 Organizzare i singoli membri in card/schede */
 
+/* ARRAY DI OGGETTI */
+const persone = [
+    {
+        nome: 'Wayne Barnett ',
+        ruolo: 'Founder & CEO ',
+        foto: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        foto: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        foto: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        foto: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer' ,
+        foto: 'scott-estrada-developer.jpg'
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Grapich Designer',
+        foto: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
